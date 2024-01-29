3a080864192b48f0348fd68442b3c532049a2239
Firat Kocabacak <firat0667@hotmail.com>
2024-01-29 22:07:54 +0300

    first release

---
+++ csharpWorks/csharpWorks/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace csharpWorks
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int current = random.Next(1, 11);
+
+            do
+            {
+                current = random.Next(1, 11);
+
+                if (current >= 8) continue;
+
+                Console.WriteLine(current);
+            } while (current != 7);
+
+
+        }
+    }
+}
